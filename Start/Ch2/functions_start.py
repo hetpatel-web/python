191e10151ded7eb6b7aa6686c1d97108857d818d
Het Patel <hetpatel.web@gmail.com>
2025-05-14 20:45:19 -0400

    functions

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,45 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+# def hello_func():
+#   name = input("What is your name? :")
+#   print("Nice to meet you,", name)
+
+# call the function
+# hello_func()
 
 # function that takes parameters
+# def hello_func2(greeting):
+#   name = input("What is your name? :")
+#   print(greeting)
+#   print("Nice to meet you,", name)
 
+# hello_func2("Greetings")
+# hello_func2("Howdy")
 
 # function that returns a value
+# def cube(x):
+#     return x * x * x
+
+# result = cube(3)
+# print(result)
 
 
 # function with default value for an parameter
+def hello_func3(greeting, name=None):
+    if name is None:
+        name = input("What is your name? :")
+    print(greeting)
+    print("Nice to meet you,", name)
 
+hello_func3("Greetings", "Joe")
 
 # function with variable number of parameters
+def add_numbers(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+result = add_numbers(1, 2, 3, 4, 5)
+print(result)
